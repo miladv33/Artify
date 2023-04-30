52628d49ba294c68c626ee98069dd49d6aa199fe
Milad Varvaei <milad.mosavi.developer@gmail.com>
2023-04-30 20:49:38 +0430

    define repository

--- app/src/main/java/com/example/artify/data/repository/MetObjectDataRepository.kt
+++ app/src/main/java/com/example/artify/data/repository/MetObjectDataRepository.kt
@@ -1,7 +1,6 @@
 package com.example.artify.data.repository
 
 import com.example.artify.data.map.mappers.ObjectDataMapper
-import com.example.artify.data.model.MetObjectData
 import com.example.artify.data.remote.MetService
 import com.example.artify.data.repository.safecall.SafeCallDelegate
 import com.example.artify.data.repository.safecall.SafeCallDelegateImpl
--- app/src/main/java/com/example/artify/di/Repository.kt
+++ app/src/main/java/com/example/artify/di/Repository.kt
@@ -1,9 +1,11 @@
 package com.example.artify.di
 
 import com.example.artify.data.map.delegate.failedmap.FailedMapperDelegate
+import com.example.artify.data.map.mappers.ObjectDataMapper
 import com.example.artify.data.map.mappers.SearchedMapper
 import com.example.artify.data.paging.PageManger
 import com.example.artify.data.remote.MetService
+import com.example.artify.data.repository.MetObjectDataRepository
 import com.example.artify.data.repository.SearchRepository
 import com.example.artify.data.repository.safecall.SafeCallDelegateImpl
 import dagger.Module
@@ -29,7 +31,18 @@ object Repository {
 
     @Provides
     @Singleton
+    fun provideMetObjectRepository(
+        metService: MetService,
+        mapper: ObjectDataMapper,
+        safeCallDelegate: SafeCallDelegateImpl
+    ): MetObjectDataRepository {
+        return MetObjectDataRepository(metService, mapper, safeCallDelegate)
+    }
+
+    @Provides
+    @Singleton
     fun provideSafeCallDelegate(failedMapperDelegate: FailedMapperDelegate): SafeCallDelegateImpl {
         return SafeCallDelegateImpl(failedMapperDelegate)
     }
+
 }
